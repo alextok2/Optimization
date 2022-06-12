9af511abc2c0059bebec2dffd951cc7ac52d50c4
alextok2 <alextok2@mail.ru>
2022-06-12 14:20:08 +0500

    3.15 и 3.16

--- Langrandge/3_15.py
+++ Langrandge/3_15.py
@@ -1,24 +1,22 @@
 import numpy as np
-
+from scipy.optimize import fsolve
 
 def func(X):
     x1 = X[0]
     x2 = X[1]
     x3 = X[2]
-    L = X[3] # this is the multiplier. lambda is a reserved keyword in python
+    L = X[3]
     G = X[4]
     return x1*x2*x3 + L * (2*x1*x2 + x2*x3 - 12) + G * (2*x1 - x2 - 8)
 
 def dfunc(X):
     dLambda = np.zeros(len(X))
-    h = 1e-3 # this is the step size used in the finite difference.
+    h = 1e-3 
     for i in range(len(X)):
         dX = np.zeros(len(X))
         dX[i] = h
         dLambda[i] = (func(X+dX)-func(X-dX))/(2*h)
     return dLambda
-
-from scipy.optimize import fsolve
 
 
 X1 = fsolve(dfunc, [1, 1, -6, 0, 0])
